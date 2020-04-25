0092a44dc88d4a087f1a613b2d5b895555c0406c
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-04-25 17:03:28 +0300

    GroupDeletionTests to TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/GroupDeletionTests.java
@@ -1,35 +1,15 @@
 package ru.stqa.jt.addressbook;
 
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class GroupDeletionTests {
-  private WebDriver wd;
-
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() {
-    wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.id("LoginForm")).submit();
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("selected[]")).click();
-    wd.findElement(By.name("delete")).click();
-    wd.findElement(By.linkText("groups")).click();
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
   }
 
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() {
-    wd.quit();
-  }
 }
