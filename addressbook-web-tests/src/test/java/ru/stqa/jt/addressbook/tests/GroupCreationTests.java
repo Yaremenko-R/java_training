78a722225a470244d2d8a7aeb9c58a793fddd0c9
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-05-01 18:42:57 +0300

    Default fild values(null) added

--- addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/tests/GroupCreationTests.java
@@ -9,7 +9,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("g1", "g1", "g1"));
+    app.getGroupHelper().fillGroupForm(new GroupData("g1", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getNavigationHelper().returnToGroupPage();
   }
