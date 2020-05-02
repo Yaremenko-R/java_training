2cbc8ae4ebb492bdd0f19b8a210d9be2e89840f8
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-05-02 16:51:39 +0300

    Optimization createGroup()

--- addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/tests/GroupCreationTests.java
@@ -8,9 +8,7 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("g1", "g1", null));
-    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().createGroup(new GroupData("g1", "g1", null));
     app.getNavigationHelper().returnToGroupPage();
   }
 }
