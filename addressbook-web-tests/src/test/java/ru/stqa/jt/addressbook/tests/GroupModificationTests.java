c57ed34dc3e6395ae2b4b9c7994e324faf09b5ac
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-05-09 18:59:33 +0300

    Groups number checking through list added

--- addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/tests/GroupModificationTests.java
@@ -4,22 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.jt.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("g3", "g3", "g3"));
     }
+    List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup(0);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("g01", "g02", "g03"));
     app.getGroupHelper().submitGroupModification();
     app.getNavigationHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
   }
 }
