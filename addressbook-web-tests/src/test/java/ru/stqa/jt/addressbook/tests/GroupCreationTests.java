865b53687ea1c00a17b3c97f337d08bb2a0b440f
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-05-10 15:01:44 +0300

    Max id calculation through lambda function added

--- addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/tests/GroupCreationTests.java
@@ -1,9 +1,8 @@
 package ru.stqa.jt.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.jt.addressbook.model.GroupData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -19,13 +18,7 @@ public class GroupCreationTests extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
