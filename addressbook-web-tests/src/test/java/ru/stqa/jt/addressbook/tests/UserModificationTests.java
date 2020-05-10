067f109405b5a9e3449f3bef0b6dc629abd802cb
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-05-10 17:04:41 +0300

    Lists comparison for contact modification added

--- addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/tests/UserModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/tests/UserModificationTests.java
@@ -3,7 +3,7 @@ package ru.stqa.jt.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.jt.addressbook.model.UserData;
-
+import java.util.Comparator;
 import java.util.List;
 
 public class UserModificationTests extends TestBase {
@@ -17,10 +17,18 @@ public class UserModificationTests extends TestBase {
     List<UserData> before = app.getContactHelper().getContactList();
     app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().initContactModification(1);
-    app.getContactHelper().fillUserForm(new UserData("Duck", "Texas", "322233", "Donald", "doe@mail.ru", null), false);
+    UserData contact = new UserData(before.get(1).getId(),"Duck", "Texas", "322233", "Donald", "doe@mail.ru", null);
+    app.getContactHelper().fillUserForm((contact), false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().returnToHomePage();
     List<UserData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(1);
+    before.add(contact);
+    Comparator<? super UserData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
