58a679501f02a4448e8f25ab331ee6e70b7d54e8
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-05-23 22:28:53 +0300

    UserCreationTests from json\xml file

--- addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/tests/UserCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jt/addressbook/tests/UserCreationTests.java
@@ -1,17 +1,56 @@
 package ru.stqa.jt.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.jt.addressbook.model.GroupData;
 import ru.stqa.jt.addressbook.model.UserData;
 import ru.stqa.jt.addressbook.model.Users;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserCreationTests extends TestBase {
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(UserData.class);
+    List<UserData> contacts = (List<UserData>) xstream.fromXML(xml);
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<UserData> contacts = gson.fromJson(json, new TypeToken<List<UserData>>(){}.getType());
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
 
   @BeforeMethod
   public void ensurePreconditions() {
@@ -21,14 +60,10 @@ public class UserCreationTests extends TestBase {
     }
   }
 
-  @Test
-  public void testUserCreation() {
+  @Test(dataProvider = "validContactsFromJson")
+  public void testUserCreation(UserData contact) {
     app.goTo().homePage();
     Users before = app.contact().all();
-    File photo = new File("src/test/resources/sova.jpg");
-    UserData contact = new UserData()
-            .withLastname("Doe").withAddress("USA")
-            .withHome("322233").withFirstname("John").withEmail("doe@mail.ru").withPhoto(photo);
     app.contact().create((contact), true);
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Users after = app.contact().all();
