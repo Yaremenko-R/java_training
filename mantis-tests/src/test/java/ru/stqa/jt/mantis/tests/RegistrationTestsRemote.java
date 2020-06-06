cf15cc4b81b4f7db2dcd597b09c31bd62dcd5d17
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-06-07 01:04:34 +0300

    RegistrationTestsRemote added

---
+++ mantis-tests/src/test/java/ru/stqa/jt/mantis/tests/RegistrationTestsRemote.java
@@ -0,0 +1,38 @@
+package ru.stqa.jt.mantis.tests;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.jt.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+public class RegistrationTestsRemote extends TestBase{
+
+  @DataProvider
+  public Iterator<Object[]> validUsers() {
+    List<Object[]> list = new ArrayList<>();
+    long now = System.currentTimeMillis();
+    String user = String.format("user%s", now);
+    String password = "password";
+    String email = String.format("user%s@localhost.localdomain", now);
+    list.add(new Object[] {user, password, email});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validUsers")
+  public void testRegistration(String user, String password, String email) throws IOException, MessagingException {
+    app.james().createUser(user, password);
+    app.registration().start(user, email);
+    List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
+    String confirmationLink = app.mail().findConfirmationLink(mailMessages, email);
+    app.registration().finish(confirmationLink, password);
+    assertTrue(app.newSession().login(user));
+  }
+}
