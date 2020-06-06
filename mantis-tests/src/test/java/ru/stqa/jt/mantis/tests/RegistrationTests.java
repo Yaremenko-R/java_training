c1ce9e3282a1749dd99dffcdb5220ed833837cb7
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-06-06 23:54:15 +0300

    DataProvider for RegistrationTests added

--- mantis-tests/src/test/java/ru/stqa/jt/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/jt/mantis/tests/RegistrationTests.java
@@ -3,29 +3,39 @@ package ru.stqa.jt.mantis.tests;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.jt.mantis.model.MailMessage;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validUsers() {
+    List<Object[]> list = new ArrayList<>();
+    long now = System.currentTimeMillis();
+    String user = String.format("user%s", now);
+    String password = "password";
+    String email = String.format("user%s@localhost.localdomain", now);
+    list.add(new Object[] {user, password, email});
+    return list.iterator();
+  }
+
   @BeforeMethod
   public void startMailServer(){
     app.mail().start();
   }
 
-  @Test
-  public void testRegistration() throws IOException, MessagingException {
-    long now = System.currentTimeMillis();
-    String user = String.format("user%s", now);
-    String password = "password";
-    String email = String.format("user%s@localhost.localdomain", now);
+  @Test(dataProvider = "validUsers")
+  public void testRegistration(String user, String password, String email) throws IOException, MessagingException {
     app.registration().start(user, email);
     List<MailMessage> mailMessages = app.mail().waitForMail(2,10000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
