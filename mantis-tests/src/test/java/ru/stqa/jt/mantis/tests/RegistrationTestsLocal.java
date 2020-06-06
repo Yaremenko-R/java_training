45048eb303379d20090dfd9ad046debbb061fb89
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-06-07 01:03:12 +0300

    RegistrationTestsLocal optimization

--- mantis-tests/src/test/java/ru/stqa/jt/mantis/tests/RegistrationTestsLocal.java
+++ mantis-tests/src/test/java/ru/stqa/jt/mantis/tests/RegistrationTestsLocal.java
@@ -1,11 +1,9 @@
 package ru.stqa.jt.mantis.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.jt.mantis.model.MailMessage;
 
 import javax.mail.MessagingException;
@@ -38,15 +36,9 @@ public class RegistrationTestsLocal extends TestBase {
   public void testRegistration(String user, String password, String email) throws IOException, MessagingException {
     app.registration().start(user, email);
     List<MailMessage> mailMessages = app.mail().waitForMail(2,10000);
-    String confirmationLink = findConfirmationLink(mailMessages, email);
+    String confirmationLink = app.mail().findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(user));
-  }
-
-  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
-    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
-    VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
-    return regex.getText(mailMessage.text);
   }
 
   @AfterMethod(alwaysRun = true)
