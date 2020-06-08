88861363ac762a6887640ff01e01d0d33f55797a
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-06-08 23:33:32 +0300

    Users class added

---
+++ mantis-tests/src/test/java/ru/stqa/jt/mantis/model/Users.java
@@ -0,0 +1,41 @@
+package ru.stqa.jt.mantis.model;
+
+import com.google.common.collect.ForwardingSet;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Users extends ForwardingSet<UserData> {
+
+  private Set<UserData> delegate;
+
+  public Users(Users contacts) {
+    this.delegate = new HashSet<>(contacts.delegate);
+  }
+
+  public Users() {
+    this.delegate = new HashSet<>();
+  }
+
+  public Users(Collection<UserData> contacts) {
+    this.delegate = new HashSet<>(contacts);
+  }
+
+  @Override
+  protected Set<UserData> delegate() {
+    return delegate;
+  }
+
+  public Users withAdded(UserData contact) {
+    Users contacts = new Users(this);
+    contacts.add(contact);
+    return contacts;
+  }
+
+  public Users without(UserData contact) {
+    Users contacts = new Users(this);
+    contacts.remove(contact);
+    return contacts;
+  }
+}
