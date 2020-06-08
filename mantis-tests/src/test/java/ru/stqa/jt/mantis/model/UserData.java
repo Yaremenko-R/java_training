3362948ce23b723a2844e959b9172836b7f5b8b1
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-06-09 01:36:36 +0300

    Hibernate configuration

--- mantis-tests/src/test/java/ru/stqa/jt/mantis/model/UserData.java
+++ mantis-tests/src/test/java/ru/stqa/jt/mantis/model/UserData.java
@@ -1,9 +1,27 @@
 package ru.stqa.jt.mantis.model;
 
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "mantis_user_table")
 public class UserData {
 
+  @Id
+  @Column(name = "id")
   public int id;
+
+  @Column(name = "username")
+  @Type(type = "text")
   public String username;
+
+  @Column(name = "email")
+  @Type(type = "text")
   public String email;
 
   public int getId() {
@@ -31,5 +49,29 @@ public class UserData {
   public UserData withEmail(String email) {
     this.email = email;
     return this;
+  }
+
+  @Override
+  public String toString() {
+    return "UserData{" +
+            "id=" + id +
+            ", username='" + username + '\'' +
+            ", email='" + email + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    UserData userData = (UserData) o;
+    return id == userData.id &&
+            Objects.equals(username, userData.username) &&
+            Objects.equals(email, userData.email);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, username, email);
   }
 }
