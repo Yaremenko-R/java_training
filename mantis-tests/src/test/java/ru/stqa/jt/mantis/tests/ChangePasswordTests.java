a8d8388d6b7d959f98046eb8ea85db69b788ab48
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-06-09 01:37:50 +0300

    ChangePasswordTests added

---
+++ mantis-tests/src/test/java/ru/stqa/jt/mantis/tests/ChangePasswordTests.java
@@ -0,0 +1,41 @@
+package ru.stqa.jt.mantis.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.jt.mantis.model.MailMessage;
+import ru.stqa.jt.mantis.model.UserData;
+import ru.stqa.jt.mantis.model.Users;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+public class ChangePasswordTests extends TestBase {
+
+  @BeforeMethod
+  public void startMailServer(){
+    app.mail().start();
+  }
+
+  @Test
+  public void testChangePassword() throws IOException, MessagingException {
+    Users mantisUsers = app.db().users();
+    UserData user = mantisUsers.iterator().next();
+    app.registration().adminLogin();
+    app.registration().resetUserPassword(user.getId());
+    List<MailMessage> mailMessages = app.mail().waitForMail(3,60000);
+    String confirmationLink = app.mail().findConfirmationLink(mailMessages, user.getEmail());
+    app.registration().finish(confirmationLink, "password");
+    assertTrue(app.newSession().login(user.toString()));
+    assertTrue(app.newSession().isLoggedInAs(user.toString()));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void stopMailServer(){
+    app.mail().stop();
+  }
+
+}
