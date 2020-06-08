fd9afd97ba5673e971c5a10bb3e21f731c3cf173
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-06-08 23:02:32 +0300

    UserData class added

---
+++ mantis-tests/src/test/java/ru/stqa/jt/mantis/model/UserData.java
@@ -0,0 +1,35 @@
+package ru.stqa.jt.mantis.model;
+
+public class UserData {
+
+  public int id;
+  public String username;
+  public String email;
+
+  public int getId() {
+    return id;
+  }
+
+  public UserData withId(int id) {
+    this.id = id;
+    return this;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public UserData withUsername(String username) {
+    this.username = username;
+    return this;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public UserData withEmail(String email) {
+    this.email = email;
+    return this;
+  }
+}
